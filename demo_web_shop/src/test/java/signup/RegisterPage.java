86ec8e475a996abfd3fc43de87f693dc4539c7b5
Aman_Chandra <Aman_Chandra@172.16.26.64>
2025-02-28 17:03:13 +0530

    Testing Second Push

---
+++ demo_web_shop/src/test/java/signup/RegisterPage.java
@@ -0,0 +1,10 @@
+package signup;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegisterPage {
+	@FindBy(id="email")
+	WebElement emailField;
+
+}
